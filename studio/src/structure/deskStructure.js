fb3ce656ca90f311deedf826709a01c55c2706f3
Prafful Indirala <52773453+praffullog@users.noreply.github.com>
2021-01-12 20:59:12 +0000

    Update deskStructure.js
    remote url https://blog.praffulindirala.com

--- studio/src/structure/deskStructure.js
+++ studio/src/structure/deskStructure.js
@@ -8,7 +8,7 @@ import {
 import IframePreview from '../previews/IframePreview'
 
 // Web preview configuration
-const remoteURL = 'https://sanity-gatsby-blog-web-c115banj.netlify.app'
+const remoteURL = 'https://blog.praffulindirala.com'
 const localURL = 'http://localhost:8000'
 const previewURL =
   window.location.hostname === 'localhost' ? localURL : remoteURL
