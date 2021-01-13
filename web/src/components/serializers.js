cbd5e09bbd2b757a935dc6d76d4fdcfd0fa739a6
Sai Prafful <prafful.log@gmail.com>
2021-01-13 15:43:07 +0000

    Added SyntaxHighlighter

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import Figure from './Figure'
+import React from "react";
+import Figure from "./Figure";
+import SyntaxHighlighter from "react-syntax-highlighter";
 
 const serializers = {
   types: {
-    authorReference: ({node}) => <span>{node.author.name}</span>,
-    mainImage: Figure
-  }
-}
+    authorReference: ({ node }) => <span>{node.author.name}</span>,
+    mainImage: Figure,
+    code: (node = {}) => {
+      const { code, language } = node;
+      if (!code) {
+        return null;
+      }
+      return <SyntaxHighlighter language={language || "text"}>{code}</SyntaxHighlighter>;
+    },
+  },
+};
 
-export default serializers
+export default serializers;
